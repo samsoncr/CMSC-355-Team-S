5776a82d45f94872a944d760c72394b905b069aa
Christopher Samson <samsoncr@mymail.vcu.edu>
2020-09-23 18:41:14 -0400

    Created a new java class called game loop. We were getting an error saying that the app was unresponsive. This is because we need to do as little computation as possible on the UI thread. I will now be looking into creating other threads that can interact with the UI.

--- TeamS355/app/src/main/java/com/cmsc355/teams/PlayingDefaultMapActivity.java
+++ TeamS355/app/src/main/java/com/cmsc355/teams/PlayingDefaultMapActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.EventLog;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,9 +17,7 @@ public class PlayingDefaultMapActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playing_default_map);
 
-        //ImageView character = (ImageView)findViewById(R.id.character);
-
-        Button playActivityBtn = (Button)findViewById(R.id.playActivityBtn);
+        Button playActivityBtn = (Button) findViewById(R.id.playActivityBtn);
         playActivityBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -26,42 +26,46 @@ public class PlayingDefaultMapActivity extends AppCompatActivity {
             }
         });
 
-        Button moveUpBtn = (Button)findViewById(R.id.moveUpBtn);
+        Button moveUpBtn = (Button) findViewById(R.id.moveUpBtn);
         moveUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ImageView character = (ImageView)findViewById(R.id.character);
-                character.setY(character.getY()-5);
+                ImageView character = (ImageView) findViewById(R.id.character);
+                character.setY(character.getY() - 5);
+
             }
         });
 
-        Button moveDownBtn = (Button)findViewById(R.id.moveDownBtn);
+        Button moveDownBtn = (Button) findViewById(R.id.moveDownBtn);
         moveDownBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ImageView character = (ImageView)findViewById(R.id.character);
-                character.setY(character.getY()+5);
+                ImageView character = (ImageView) findViewById(R.id.character);
+                character.setY(character.getY() + 5);
+
             }
         });
 
-        Button moveLeftBtn = (Button)findViewById(R.id.moveLeftBtn);
+        Button moveLeftBtn = (Button) findViewById(R.id.moveLeftBtn);
         moveLeftBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ImageView character = (ImageView)findViewById(R.id.character);
-                character.setX(character.getX()-5);
+                ImageView character = (ImageView) findViewById(R.id.character);
+                character.setX(character.getX() - 5);
             }
         });
 
-        Button moveRightBtn = (Button)findViewById(R.id.moveRightBtn);
+        Button moveRightBtn = (Button) findViewById(R.id.moveRightBtn);
         moveRightBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                ImageView character = (ImageView)findViewById(R.id.character);
-                character.setX(character.getX()+5);
+                ImageView character = (ImageView) findViewById(R.id.character);
+                character.setX(character.getX() + 5);
             }
         });
 
+
     }
+
 
 }
