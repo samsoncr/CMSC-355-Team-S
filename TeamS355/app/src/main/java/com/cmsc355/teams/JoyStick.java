286a3141a8e0596e8eb043a39c0611073f5be594
JeffreyTucker <tuckerjs2@vcu.edu>
2020-10-08 22:40:45 -0500

    Integrate joystick & player classes into main project
    -Add new color ids on colors.xml for UPS and FPS tracker string display.
    -Integrate Joystick controller class into main project code.
    -Integrate player class into main project code.
    -Integrate GameLoop class into main project code.

---
+++ TeamS355/app/src/main/java/com/cmsc355/teams/JoyStick.java
@@ -0,0 +1,111 @@
+package com.cmsc355.teams;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+public class JoyStick {
+    private Paint innerCirclePaint;
+    private Paint outerCirclePaint;
+    private int innerCircleRadius;
+    private int outerCircleRadius;
+    private int outerCircleCenterPositionX;
+    private int outerCircleCenterPositionY;
+    private int innerCircleCenterPositionX;
+    private int innerCircleCenterPositionY;
+    private double joyStickCenterToTouchDistance;
+    private boolean isPressed;
+    private double actuatorX;
+    private double actuatorY;
+
+    public JoyStick(int centerPositionX, int centerPositionY, int outerCircleRadius, int innerCircleRadius){
+
+        // Outer and inner circle make up the joystick
+        outerCircleCenterPositionX = centerPositionX;
+        outerCircleCenterPositionY = centerPositionY;
+        innerCircleCenterPositionX = centerPositionX;
+        innerCircleCenterPositionY = centerPositionY;
+
+        // Radius of circles
+        this.outerCircleRadius = outerCircleRadius;
+        this.innerCircleRadius = innerCircleRadius;
+
+        // Color paint of circles
+        outerCirclePaint = new Paint();
+        outerCirclePaint.setColor(Color.GRAY);
+        outerCirclePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        innerCirclePaint = new Paint();
+        innerCirclePaint.setColor(Color.BLUE);
+        innerCirclePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+    }
+
+    public void draw(Canvas canvas) {
+
+        //draw outer circle
+        canvas.drawCircle(
+                outerCircleCenterPositionX,
+                outerCircleCenterPositionY,
+                outerCircleRadius,
+                outerCirclePaint);
+
+        //draw inner circle
+        canvas.drawCircle(
+                innerCircleCenterPositionX,
+                innerCircleCenterPositionY,
+                innerCircleRadius,
+                innerCirclePaint);
+    }
+
+    public void update() {
+        updateInnerCirclePosition();
+    }
+
+    private void updateInnerCirclePosition() {
+        innerCircleCenterPositionX = (int) (outerCircleCenterPositionX + actuatorX * outerCircleRadius);
+        innerCircleCenterPositionY = (int) (outerCircleCenterPositionY + actuatorY * outerCircleRadius);
+    }
+
+    public boolean isPressed(double touchPositionX, double touchPositionY) {
+        joyStickCenterToTouchDistance = Math.sqrt(
+                Math.pow(outerCircleCenterPositionX - touchPositionX, 2) +
+                        Math.pow(outerCircleCenterPositionY - touchPositionY, 2));
+        return joyStickCenterToTouchDistance < outerCircleRadius;
+    }
+
+    public void setIsPressed(boolean isPressed) {
+        this.isPressed = isPressed;
+    }
+
+    public boolean getIsPressed() {
+        return isPressed;
+    }
+
+    public void setActuator(double touchPositionX, double touchPositionY) {
+        double deltaX = touchPositionX - outerCircleCenterPositionX;
+        double deltaY = touchPositionY - outerCircleCenterPositionY;
+        double deltaDistance = Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
+
+        if(deltaDistance < outerCircleRadius){
+            actuatorX = deltaX/outerCircleRadius;
+            actuatorY = deltaY/outerCircleRadius;
+        }
+        else{
+            actuatorX = deltaX/deltaDistance;
+            actuatorY = deltaY/deltaDistance;
+        }
+    }
+
+    public void resetActuator() {
+        actuatorX = 0.0;
+        actuatorY = 0.0;
+    }
+
+    public double getActuatorX() {
+        return actuatorX;
+    }
+
+    public double getActuatorY() {
+        return actuatorY;
+    }
+}
