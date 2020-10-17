f5ad5ef05efc694b803453fc32ef6b3ea60ddcc5
JeffreyTucker <tuckerjs2@vcu.edu>
2020-10-16 22:57:46 -0500

    Update Player.java
    Collsion update:
    -Define screen boundary for x and y axis so player character doesn't disappear off the screen.
    *y-axis boundary doesn't work correctly. Will be fixed in near future update.

--- TeamS355/app/src/main/java/com/cmsc355/teams/Player.java
+++ TeamS355/app/src/main/java/com/cmsc355/teams/Player.java
@@ -1,14 +1,20 @@
 package com.cmsc355.teams;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
+import android.view.Display;
+import android.view.WindowManager;
 
 import androidx.core.content.ContextCompat;
 
 public class Player {
     private static final double SPEED_PIXELS_PER_SECOND = 500.0; //max speed of character movement
     private static final double MAX_SPEED = SPEED_PIXELS_PER_SECOND / GameLoop.MAX_UPS;
+    private final double width;
+    private final double height;
     private double positionX;
     private double positionY;
     private double radius;
@@ -24,17 +30,36 @@ public class Player {
         paint = new Paint();
         int color = ContextCompat.getColor(context, R.color.player);
         paint.setColor(color);
+
+        WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        //Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        this.width = size.x;
+        this.height = size.y;
+
+
     }
+
     public void draw(Canvas canvas) {
         canvas.drawCircle((float)positionX, (float) positionY, (float) radius, paint);
+
 
     }
 
     public void update(JoyStick joyStick) {
+
         velocityX = joyStick.getActuatorX()*MAX_SPEED;
         velocityY = joyStick.getActuatorY()*MAX_SPEED;
-        positionX += velocityX;
-        positionY += velocityY;
+        if((velocityX + positionX) < width && (velocityX + positionX) > 0){
+            positionX += velocityX;
+        }
+        if((velocityY + positionY) > height && (velocityY + positionY) < 0){
+            positionY += velocityY;
+        }
+        //positionX += velocityX;
+        //positionY += velocityY;
     }
 
     public void setPosition(double positionX, double positionY) {
