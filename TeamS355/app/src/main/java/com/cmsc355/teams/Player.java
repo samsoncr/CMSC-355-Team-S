425ecf563e49998dcb7d1b02555bbd5f5dfaeea6
Christopher Samson <samsoncr@mymail.vcu.edu>
2020-10-28 20:51:41 -0400

    fourth attempt on CI on only my branch

--- TeamS355/app/src/main/java/com/cmsc355/teams/Player.java
+++ TeamS355/app/src/main/java/com/cmsc355/teams/Player.java
@@ -13,7 +13,7 @@ import android.view.WindowManager;
 import androidx.core.content.ContextCompat;
 import java.util.ArrayList;
 
-//Test number 3 for Continuous Integration On ChristopherSamson branch
+//Test number 4 for Continuous Integration On ChristopherSamson branch
 
 public class Player {
     private static final double SPEED_PIXELS_PER_SECOND = 500.0; //max speed of character movement
